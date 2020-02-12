ff3555f08a41595db524857483962a0292133311
imran110219 <imran110219@gmail.com>
2020-02-12 23:24:47 +0600

    1. Dynamic Output for filling missing values.

--- Clean_Dataset.py
+++ Clean_Dataset.py
@@ -1,12 +1,9 @@
 from Load_Dataset import load_dataset
 
 import numpy as obj_numpy
-from functools import reduce
-import pandas as obj_pandas
 
-
-def clean():
-    weather_data = load_dataset("Datasets/Weather(Khulna).csv")
+def clean(data="Datasets/Weather(Khulna).csv", type="mean"):
+    weather_data = load_dataset(data)
     # Handling missing value(in this data, '****' is represented as null values)
     print(weather_data.loc[weather_data['mintemp'] == '****'].count())
     weather_data = weather_data.replace('****', obj_numpy.nan)
@@ -25,11 +22,21 @@ def clean():
          "maxtemp": float,
          "mintemp": float})
 
-    # Replace all the null values with their mean values
-    weather_data["sunshine"] = weather_data.sunshine.fillna(round(weather_data['sunshine'].mean(), 1))
-    weather_data["maxtemp"] = weather_data.maxtemp.fillna(round(weather_data['maxtemp'].mean(), 1))
-    weather_data["mintemp"] = weather_data.mintemp.fillna(round(weather_data['mintemp'].mean(), 1))
-    print(weather_data)
+    if type == "mean":
+        # Replace all the null values with their mean values
+        weather_data["sunshine"] = weather_data.sunshine.fillna(round(weather_data['sunshine'].mean(), 1))
+        weather_data["maxtemp"] = weather_data.maxtemp.fillna(round(weather_data['maxtemp'].mean(), 1))
+        weather_data["mintemp"] = weather_data.mintemp.fillna(round(weather_data['mintemp'].mean(), 1))
+    elif type == "delete":
+        weather_data.drop(weather_data[weather_data['sunshine'] == obj_numpy.nan].index, inplace = True)
+        weather_data.drop(weather_data[weather_data['maxtemp'] == obj_numpy.nan].index, inplace = True)
+        weather_data.drop(weather_data[weather_data['mintemp'] == obj_numpy.nan].index, inplace = True)
+    elif type == "median":
+        # Replace all the null values with their mean values
+        weather_data["sunshine"] = weather_data.sunshine.fillna(round(weather_data['sunshine'].median(), 1))
+        weather_data["maxtemp"] = weather_data.maxtemp.fillna(round(weather_data['maxtemp'].median(), 1))
+        weather_data["mintemp"] = weather_data.mintemp.fillna(round(weather_data['mintemp'].median(), 1))
 
+    print(weather_data)
 
 clean()
